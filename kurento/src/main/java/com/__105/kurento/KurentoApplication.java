4a0d7397cb75f427c8f57f32b9a6f6b367b422dc
sky4tony <sky4tony@naver.com>
2024-07-21 03:42:17 +0900

    create signaling

---
+++ kurento/src/main/java/com/__105/kurento/KurentoApplication.java
@@ -0,0 +1,13 @@
+package com.__105.kurento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KurentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KurentoApplication.class, args);
+	}
+
+}
---
+++ kurento/src/test/java/com/__105/kurento/KurentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.__105.kurento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KurentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
